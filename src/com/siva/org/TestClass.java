aedc7b2740c5a6689e1008edf5668ff7686d84a1
Santhosh Gampa <santhug435@gmail.com>
2025-09-09 13:35:20 +0530

    Adding initial classes

---
+++ src/com/siva/org/TestClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.siva.org;
+
+/**
+ * 
+ */
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
